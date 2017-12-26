389d432a7e6ff7e53cbbc7c43b107c134b27eeca
Alex McCulloch <alexmccx@gmail.com>
2017-12-26 12:16:03 -0500

    added logout button

--- app/src/main/java/com/example/alexm/selfbet/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/alexm/selfbet/fragments/SettingsFragment.java
@@ -1,13 +1,26 @@
 package com.example.alexm.selfbet.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.alexm.selfbet.LoginActivity;
 import com.example.alexm.selfbet.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingsFragment extends Fragment {
+    @BindView(R.id.btn_logout) Button logoutButton;
+    private View view;
+
+    private FirebaseAuth mAuth;
+
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
         return fragment;
@@ -21,6 +34,22 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
+
 }
