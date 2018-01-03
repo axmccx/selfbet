af1550bd1afd3669aab369bda1b4f93fd3a01907
Alex McCulloch <alexmccx@gmail.com>
2018-01-03 09:48:31 -0500

    Can place bets

--- app/src/main/java/com/example/alexm/selfbet/PlaceBetActivity.java
+++ app/src/main/java/com/example/alexm/selfbet/PlaceBetActivity.java
@@ -1,9 +1,18 @@
 package com.example.alexm.selfbet;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,5 +27,32 @@ public class PlaceBetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_bet);
         ButterKnife.bind(this);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String groupName = "Test2";
+                String type = "mock";
+                String amount = betAmount.getText().toString();
+                placeBet(groupName, type, amount);
+                finish();
+            }
+        });
+    }
+
+    protected void placeBet(final String groupName, final String type, final String amount) {
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mUser.getToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+            public void onComplete(@NonNull Task<GetTokenResult> task) {
+                if (task.isSuccessful()) {
+                    String idToken = task.getResult().getToken();
+                    new cloudFuncHTTP(getApplicationContext())
+                            .execute(idToken, cloudFuncHTTP.PLACE_BET_FUNC, groupName, type, amount);
+                } else {
+                    Toast.makeText(getApplicationContext(), "AUTH ERROR", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -22,8 +22,8 @@ exports.createGroup = functions.https.onRequest((request, response) => {
 
 function createGroupInDB(uid, groupName, response) {
 	const db = admin.firestore();
-	const userRef = db.collection('users').doc(uid)
-	const groupRef = db.collection('groups').doc(groupName) 
+	const userRef = db.collection('users').doc(uid);
+	const groupRef = db.collection('groups').doc(groupName);
 
 	// check whether the group already exists...
 	groupRef.get().then((docSnapshot) => {
@@ -68,8 +68,8 @@ exports.joinGroup = functions.https.onRequest((request, response) => {
 
 function joinGroupInDB(uid, groupName, response) {
 	const db = admin.firestore();
-	const userRef = db.collection('users').doc(uid)
-	const groupRef = db.collection('groups').doc(groupName) 
+	const userRef = db.collection('users').doc(uid);
+	const groupRef = db.collection('groups').doc(groupName);
 	groupRef.get().then((docSnapshot) => {
 		if (docSnapshot.exists) {
 			// get current members array of the specified group, and append the uid...
@@ -92,4 +92,36 @@ function joinGroupInDB(uid, groupName, response) {
 			console.log('ERROR: Trying to join ' + groupName + ', but it doesn\'t already exists');
 		}
 	});
+}
+
+exports.placeBet = functions.https.onRequest((request, response) => {
+	console.log('placeBet called');
+	if (!request.headers.authorization) {
+		console.error('No Firebase ID token was passed');
+		response.status(403).send('Unauthorized');
+		return;
+	}
+	admin.auth().verifyIdToken(request.headers.authorization).then(decodedIdToken => {
+		console.log('ID Token correctly decoded', decodedIdToken);
+		placeBetInDB(decodedIdToken.uid, request.query.groupName, request.query.type, request.query.amount, response)
+	}).catch(error => {
+		console.error('Error while verifying Firebase ID token:', error);
+		response.status(403).send('Unauthorized');
+	});
+});
+
+function placeBetInDB(uid, nGroupName, nType, nAmount, response) {
+	const db = admin.firestore();
+	const betsRef = db.collection('users').doc(uid).collection('bets');
+
+	betsRef.add({
+	    amount: nAmount,
+	    group: nGroupName,
+	    type: nType
+	}).catch(error => {
+		console.log(error);
+		response.send('An error occured');
+	});
+	console.log('Bet has been placed');
+	response.send('Bet has been placed');
 }
