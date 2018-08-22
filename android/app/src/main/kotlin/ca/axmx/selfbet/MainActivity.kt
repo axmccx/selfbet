c5dc8e69b63f97141f6af2042c6901763d896aac
Alex McCulloch <alexmccx@gmail.com>
2018-08-22 15:25:48 -0400

    Good demo build. Updated firestore, no alarm code

--- android/app/src/main/kotlin/ca/axmx/selfbet/MainActivity.kt
+++ android/app/src/main/kotlin/ca/axmx/selfbet/MainActivity.kt
@@ -3,24 +3,11 @@ package ca.axmx.selfbet
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity() {
-  private val CHANNEL = "selfbet.axmx.ca"
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-
-    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
-
-        val hour: Int = call.argument<Int>("hour")
-        val minutes: Int = call.argument<Int>("minutes")
-
-        if (call.method == "setAlarm")  {
-            result.success("Native code sets the alarm for $hour:$minutes")
-        }
+class MainActivity(): FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
     }
-  }
 }
--- lib/repositories/native_code_repo.dart
+++ lib/repositories/native_code_repo.dart
@@ -1,19 +1,38 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:vibrate/vibrate.dart';
 import 'package:selfbet/models/models.dart';
 
 class NativeCodeRepo {
-  static const platform = const MethodChannel("selfbet.axmx.ca");
+  //static const _channel = const MethodChannel("selfbet.axmx.ca");
 
-  Future<void> setAlarm(Bet bet) async {
-    try {
-      platform.invokeMethod("setAlarm", bet.options).then((value) {
-        debugPrint(value);
-      });
-    } catch (e) {
-      print(e);
-    }
-
-  }
+//  Future<void> setAlarm(Bet bet) async {
+//    // determine delay from bet.options
+//    // using hard coded 10 seconds for testing
+//    final Duration delay = const Duration(seconds: 10);
+//    final int now = new DateTime.now().millisecondsSinceEpoch;
+//    final int time = now + delay.inMilliseconds;
+//
+//    _channel.setMethodCallHandler((MethodCall call) {
+//      switch (call.method) {
+//        case 'firedWhileApplicationRunning':
+//          alarmAlert();
+//          return;
+//        default:
+//          throw 'not implemented';
+//      }
+//    });
+//    final dynamic result = await _channel.invokeMethod(
+//        'setAlarm',
+//        [0, time, "alarmAlert"]
+//    );
+//    return (result == null) ? false : result;
+//  }
+//
+//  void alarmAlert() {
+//    debugPrint("Sounds alarm!! Ring ring!");
+//    SystemSound.play(SystemSoundType.click);
+//    Vibrate.vibrate();
+//  }
 }
